b773e0ce2e65a75031e14e5df8d25642d3ebc069
saisd2 <saisd2@illinois.edu>
2022-09-30 22:22:31 -0500

    created frontend and seperated backend

--- django-test/webapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UserProfile
-
-# Convert UserProfile to JSON
-class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = ('first_name', 'last_name', 'pronouns', 'calories_burnt')
