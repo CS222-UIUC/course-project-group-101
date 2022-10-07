d75e1451abdb5b3a8a9d144cc88165feb1be23b8
Gloria Li <glorial4@illinois.edu>
2022-10-06 21:37:56 -0500

    Changed up the styling to look better

--- frontend/calorie-clash/src/App.js
+++ frontend/calorie-clash/src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
+import './stylesheets/App.css';
+import './stylesheets/general.css';
 
 function App() {
   return (
     <div className="App">
-      <div id="navbar" class="sticky">
-          <a href="home"> Home </a>
-          <a href="leaderboard"> Leaderboard </a>
-          <a href="profile"> Profile </a>
+      <div id="navbar" className="sticky">
+          {/*Add the appropriate links when the pages are created*/}
+          <a href="App"> Leaderboard </a>
+          <a href="App"> Profile </a>
+          <a href="App"> About </a>
+          <a href="App"> Home </a>
       </div>
-      <div id="container" class="center">
-          <h1 class="center projectname"> Project Name Here </h1>
-          <div class="center content">
-              {/*Put welcome message + basic info about web app*/}
-              <p class="text">
-                  Info Here
-              </p>
-          </div>
+      <div id="title" className="center">
+          {/*We can put a logo here when we have one*/}
+          <h1 className="center projectname"> Calorie Clash </h1>
+      </div>
+      {/*Put welcome message + basic info about web app*/}
+      <div className="center content">
+        <p className="text">
+            Welcome to Calorie Clash! <br></br><br></br>
+
+            To learn more about how to use this site click below <br></br>
+        </p>
       </div>
     </div>
   );
