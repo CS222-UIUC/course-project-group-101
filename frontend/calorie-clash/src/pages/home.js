cd3a77dda1440bf62a422bc665eb706598671a1d
saisd2 <saisd2@illinois.edu>
2022-10-14 13:10:55 -0500

    broken functionality with navbar

--- frontend/calorie-clash/src/pages/home.js
+++ frontend/calorie-clash/src/pages/home.js
@@ -7,8 +7,36 @@ import React from 'react';
   
 const Home = () => {
   return (
-    <div>
-      <h1>Learn more here!</h1>
+    <div className="App">
+      <div id="navbar" className="sticky">
+          {/*Add the appropriate links when the pages are created*/}
+          <a href="leaderboard"> Leaderboard </a>
+          <a href="profile"> Profile </a>
+          <a href="get-started"> Getting Started </a>
+          <a href="home"> Home </a>
+      </div>
+      <div id="title" className="center">
+          {/*We can put a logo here when we have one*/}
+          <h1 className="center projectname"> Calorie Clash </h1>
+      </div>
+      {/*Put welcome message + basic info about web app*/}
+      <div className="center content">
+        <p className="text">
+            Welcome to Calorie Clash! <br></br><br></br>
+
+            Are you having trouble getting into your workout groove? <br></br><br></br>
+
+            No worries because Calorie Clash has your back! <br></br><br></br>
+
+            Calorie Clash can help you track workouts and calories burned and 
+            find and match individuals as “rivals” to bolster fitness competition and comradery where many cannot find it. <br></br><br></br>
+
+
+            Interested? Click below to learn more! <br></br><br></br>
+
+            <button id="started" onClick={gettingStarted}>Get Started</button>
+        </p>
+      </div>
     </div>
   );
 };
