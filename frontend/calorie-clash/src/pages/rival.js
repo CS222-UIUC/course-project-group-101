16fe61a00de70835be85a476b71a6ed077382c49
saisd2 <saisd2@illinois.edu>
2022-12-07 16:57:03 -0600

    get request for rival implemented

--- frontend/calorie-clash/src/pages/rival.js
+++ frontend/calorie-clash/src/pages/rival.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink as Link } from "react-router-dom";
 import '../stylesheets/App.css';
 import '../stylesheets/general.css';
 
@@ -18,7 +17,7 @@ class Rival extends React.Component {
     fetch(url).then((response) => response.json()).then((json) => {
         this.setState({data: json});
         var works = this.state.data.map((d) => {
-            this.setState({rival : d.rival});
+            this.setState({rival : d.partner});
             return true;
         })
         this.setState({success: works});
@@ -30,27 +29,29 @@ class Rival extends React.Component {
   };
 
   render () {
-    <div className="App">
-      <div id="title" className="center">
-          {/*We can put a logo here when we have one*/}
-          <h1 className="center projectname"> Rival </h1>
+    return (
+      <div className="App">
+        <div id="title" className="center">
+            {/*We can put a logo here when we have one*/}
+            <h1 className="center projectname"> Rival </h1>
+        </div>
+        {/*Put welcome message + basic info about web app*/}
+        <div className="center content">
+          <p className="text">
+
+              What's a rival? <br></br><br></br>
+
+              Rivals are other users on our platform that we found would be a good fit for you based on your profile <br></br><br></br>
+
+              You and your rival can compete and encourage each other to reach your goals! <br></br><br></br>
+
+              Ready to take on a Rival? Click below to find out who yours is! <br></br>
+
+              <button onClick={this.onButtonClickHandler}>Enter</button>
+          </p>
+        </div>
       </div>
-      {/*Put welcome message + basic info about web app*/}
-      <div className="center content">
-        <p className="text">
-
-            What's a rival? <br></br><br></br>
-
-            Rivals are other users on our platform that we found would be a good fit for you based on your profile <br></br><br></br>
-
-            You and your rival can compete and encourage each other to reach your goals! <br></br><br></br>
-
-            Ready to take on a Rival? Click below to find out who yours is! <br></br>
-
-            <button onClick={this.onButtonClickHandler}>Enter</button>
-        </p>
-      </div>
-    </div>
+    )
   };
 };
   
