788cab2a84f2fac2a395e0085267c9d26ab59b2b
Gloria Li <glorial4@illinois.edu>
2022-12-07 17:02:21 -0600

    Added dropdown menu for level preference

--- frontend/calorie-clash/src/forms/edit-profile.js
+++ frontend/calorie-clash/src/forms/edit-profile.js
@@ -10,7 +10,7 @@ class EditProfile extends React.Component {
     constructor(props) {
         super(props);
         //const data = JSON.parse(response.json());
-        this.state = {f_name: "f", l_name: "l", weight: 0, h_ft: 0, h_in: 0, pronouns: "", data: [], success: false};
+        this.state = {f_name: "f", l_name: "l", weight: 0, h_ft: 0, h_in: 0, pronouns: "", l_pref: "noob", data: [], success: false};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -21,7 +21,7 @@ class EditProfile extends React.Component {
         fetch(url).then((response) => response.json()).then((json) => {
             this.setState({data: json});
             var works = this.state.data.map((d) => {
-                this.setState({f_name: d.first_name, l_name: d.last_name, weight: d.weight, h_ft: d.height_ft, h_in: d.height_in, pronouns: d.pronouns});
+                this.setState({f_name: d.first_name, l_name: d.last_name, weight: d.weight, h_ft: d.height_ft, h_in: d.height_in, pronouns: d.pronouns, l_pref: d.level_pref});
                 return true;
             })
             this.setState({success: works});
@@ -48,7 +48,7 @@ class EditProfile extends React.Component {
                 "weight": this.state.weight,
                 "height_ft": this.state.h_ft,
                 "height_in": this.state.h_in,
-                "calories_burned_today": 100
+                "level_pref": this.state.l_pref
             })
         };
         var url = 'http://127.0.0.1:8000/userprofile/' + window.localStorage.getItem("UID") + '/';
@@ -61,6 +61,22 @@ class EditProfile extends React.Component {
 
     //What the form looks like
     render() {
+        let option1;
+        let option2;
+        let option3;
+        if(this.l_pref === "noob") {
+            option1 = <option value="noob" selected>Beginner</option>;
+            option2 = <option value="intm">Intermediate</option>;
+            option3 = <option value="adv">Advanced</option>;
+        } else if (this.l_pref === "intm"){
+            option1 = <option value="noob">Beginner</option>;
+            option2 = <option value="intm" selected>Intermediate</option>;
+            option3 = <option value="adv">Advanced</option>;
+        } else {
+            option1 = <option value="noob">Beginner</option>;
+            option2 = <option value="intm">Intermediate</option>;
+            option3 = <option value="adv" selected>Advanced</option>;
+        }
         return (
             <div className='App'>
                 <div id="title" class="center">
@@ -86,6 +102,14 @@ class EditProfile extends React.Component {
                         <label for = "h_in"> Height (in): </label>  
                         <input name = "h_in" type="number" min = "0" value={this.state.h_in} onChange={this.handleChange} /> <br></br>  
 
+                        <p className='text'> Note: You need to submit preferences to get a rival! </p>
+                        
+                        <label for = "preference"> Height (in): </label>  
+                        <select name = "level_pref" onChange={this.handleChange}>
+                            {option1}
+                            {option2}
+                            {option3}
+                        </select>
                         <input type="submit" value="Submit" />
                     </form>
                 </div>
