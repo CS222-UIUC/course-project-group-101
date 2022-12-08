8feefceaa1ebdaaf984903958de6c908b8a5402a
Gloria Li <glorial4@illinois.edu>
2022-12-07 18:39:04 -0600

    Added current user stats to leaderboard

--- frontend/calorie-clash/src/pages/leaderboard.js
+++ frontend/calorie-clash/src/pages/leaderboard.js
@@ -10,9 +10,7 @@ import '../stylesheets/general.css';
 class Leaderboard extends Component{
   constructor(props) {
     super(props);
-    this.state = {
-      data:[]
-    };
+    this.state = {data:[], c_burn: 0, total: 0, udata: [], success: false};
   }
 
   // Sends a request to API to get leaderboard
@@ -30,6 +28,19 @@ class Leaderboard extends Component{
   // Fetches data immediately during initialization
   componentDidMount() {
     this.getLeaderboard();
+
+    //Gets current user stats
+    if(window.localStorage.getItem("UID") !== null) {
+      var url = 'http://127.0.0.1:8000/userprofile/' + window.localStorage.getItem("UID") + '/';
+        fetch(url).then((response) => response.json()).then((json) => {
+            this.setState({udata: json});
+            var works = this.state.udata.map((d) => {
+                this.setState({c_burn: d.calories_burned_today, total: d.total_calories_burned});
+                return true;
+            })
+            this.setState({success: works});
+        });
+    }
   }
 
   render() {
@@ -49,8 +60,7 @@ class Leaderboard extends Component{
     if(window.localStorage.getItem("UID") === null) {
       stats = <p className="text">Login to view your stats.</p>
     } else {
-      //TODO: Change to current user stats
-      stats = <p className="text">Calories burned: </p>
+      stats = <p className="text">Calories burned today: {this.state.c_burn} <br></br> <br></br> Total Calories Burned: {this.state.total}</p>
     }
 
     return (
